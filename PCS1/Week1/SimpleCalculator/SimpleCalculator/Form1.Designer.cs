2d0fd747c171d78c59fceb65c4e54df39fc99f76
Meyra Al Saleh <meyrasaleh@gmail.com>
2025-02-23 16:00:25 +0200

    PCS1-week1 simple calculator assignment completion

--- PCS1/Week1/SimpleCalculator/SimpleCalculator/Form1.Designer.cs
+++ PCS1/Week1/SimpleCalculator/SimpleCalculator/Form1.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.lblFirstNumber = new System.Windows.Forms.Label();
-            this.lblSecondNumber = new System.Windows.Forms.Label();
-            this.lblAnswer = new System.Windows.Forms.Label();
-            this.tbFirstNumber = new System.Windows.Forms.TextBox();
-            this.tbSecondNumber = new System.Windows.Forms.TextBox();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnSubtract = new System.Windows.Forms.Button();
-            this.btnMultiply = new System.Windows.Forms.Button();
             this.btnDivide = new System.Windows.Forms.Button();
-            this.btnShowInfo = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbSecondNumber = new System.Windows.Forms.TextBox();
+            this.tbFirstNumber = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblSecondNumber = new System.Windows.Forms.Label();
+            this.lblFirstNumber = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tbInfo = new System.Windows.Forms.TextBox();
             this.btnHideInfo = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnShowInfo = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -63,36 +63,72 @@
             this.panel1.Size = new System.Drawing.Size(997, 727);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // btnDivide
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(222)))), ((int)(((byte)(184)))));
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.btnHideInfo);
-            this.panel2.Controls.Add(this.btnShowInfo);
-            this.panel2.Location = new System.Drawing.Point(1084, 42);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(623, 726);
-            this.panel2.TabIndex = 1;
+            this.btnDivide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
+            this.btnDivide.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(737, 336);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(122, 89);
+            this.btnDivide.TabIndex = 8;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
             // 
-            // lblFirstNumber
+            // btnMultiply
             // 
-            this.lblFirstNumber.AutoSize = true;
-            this.lblFirstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFirstNumber.Location = new System.Drawing.Point(126, 83);
-            this.lblFirstNumber.Name = "lblFirstNumber";
-            this.lblFirstNumber.Size = new System.Drawing.Size(225, 39);
-            this.lblFirstNumber.TabIndex = 0;
-            this.lblFirstNumber.Text = "First number: ";
+            this.btnMultiply.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
+            this.btnMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(537, 336);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(122, 89);
+            this.btnMultiply.TabIndex = 7;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMultiply.UseVisualStyleBackColor = false;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
             // 
-            // lblSecondNumber
+            // btnSubtract
             // 
-            this.lblSecondNumber.AutoSize = true;
-            this.lblSecondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSecondNumber.Location = new System.Drawing.Point(76, 203);
-            this.lblSecondNumber.Name = "lblSecondNumber";
-            this.lblSecondNumber.Size = new System.Drawing.Size(275, 39);
-            this.lblSecondNumber.TabIndex = 1;
-            this.lblSecondNumber.Text = "Second number: ";
+            this.btnSubtract.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
+            this.btnSubtract.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(337, 336);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(122, 89);
+            this.btnSubtract.TabIndex = 6;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = false;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(137, 336);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(122, 89);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbSecondNumber
+            // 
+            this.tbSecondNumber.Location = new System.Drawing.Point(366, 203);
+            this.tbSecondNumber.Name = "tbSecondNumber";
+            this.tbSecondNumber.Size = new System.Drawing.Size(307, 38);
+            this.tbSecondNumber.TabIndex = 4;
+            // 
+            // tbFirstNumber
+            // 
+            this.tbFirstNumber.Location = new System.Drawing.Point(366, 83);
+            this.tbFirstNumber.Name = "tbFirstNumber";
+            this.tbFirstNumber.Size = new System.Drawing.Size(307, 38);
+            this.tbFirstNumber.TabIndex = 3;
             // 
             // lblAnswer
             // 
@@ -104,78 +140,47 @@
             this.lblAnswer.TabIndex = 2;
             this.lblAnswer.Text = "Answer: ";
             // 
-            // tbFirstNumber
+            // lblSecondNumber
             // 
-            this.tbFirstNumber.Location = new System.Drawing.Point(366, 83);
-            this.tbFirstNumber.Name = "tbFirstNumber";
-            this.tbFirstNumber.Size = new System.Drawing.Size(307, 38);
-            this.tbFirstNumber.TabIndex = 3;
+            this.lblSecondNumber.AutoSize = true;
+            this.lblSecondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecondNumber.Location = new System.Drawing.Point(76, 203);
+            this.lblSecondNumber.Name = "lblSecondNumber";
+            this.lblSecondNumber.Size = new System.Drawing.Size(275, 39);
+            this.lblSecondNumber.TabIndex = 1;
+            this.lblSecondNumber.Text = "Second number: ";
             // 
-            // tbSecondNumber
+            // lblFirstNumber
             // 
-            this.tbSecondNumber.Location = new System.Drawing.Point(366, 203);
-            this.tbSecondNumber.Name = "tbSecondNumber";
-            this.tbSecondNumber.Size = new System.Drawing.Size(307, 38);
-            this.tbSecondNumber.TabIndex = 4;
+            this.lblFirstNumber.AutoSize = true;
+            this.lblFirstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstNumber.Location = new System.Drawing.Point(126, 83);
+            this.lblFirstNumber.Name = "lblFirstNumber";
+            this.lblFirstNumber.Size = new System.Drawing.Size(225, 39);
+            this.lblFirstNumber.TabIndex = 0;
+            this.lblFirstNumber.Text = "First number: ";
             // 
-            // btnAdd
+            // panel2
             // 
-            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
-            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(83, 336);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(122, 89);
-            this.btnAdd.TabIndex = 5;
-            this.btnAdd.Text = "+";
-            this.btnAdd.UseVisualStyleBackColor = false;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(222)))), ((int)(((byte)(184)))));
+            this.panel2.Controls.Add(this.tbInfo);
+            this.panel2.Controls.Add(this.btnHideInfo);
+            this.panel2.Controls.Add(this.btnShowInfo);
+            this.panel2.Location = new System.Drawing.Point(1084, 42);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(623, 726);
+            this.panel2.TabIndex = 1;
             // 
-            // btnSubtract
+            // tbInfo
             // 
-            this.btnSubtract.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
-            this.btnSubtract.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubtract.Location = new System.Drawing.Point(273, 336);
-            this.btnSubtract.Name = "btnSubtract";
-            this.btnSubtract.Size = new System.Drawing.Size(122, 89);
-            this.btnSubtract.TabIndex = 6;
-            this.btnSubtract.Text = "-";
-            this.btnSubtract.UseVisualStyleBackColor = false;
-            // 
-            // btnMultiply
-            // 
-            this.btnMultiply.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
-            this.btnMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMultiply.Location = new System.Drawing.Point(476, 336);
-            this.btnMultiply.Name = "btnMultiply";
-            this.btnMultiply.Size = new System.Drawing.Size(122, 89);
-            this.btnMultiply.TabIndex = 7;
-            this.btnMultiply.Text = "*";
-            this.btnMultiply.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnMultiply.UseVisualStyleBackColor = false;
-            // 
-            // btnDivide
-            // 
-            this.btnDivide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(218)))), ((int)(((byte)(231)))));
-            this.btnDivide.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDivide.Location = new System.Drawing.Point(685, 336);
-            this.btnDivide.Name = "btnDivide";
-            this.btnDivide.Size = new System.Drawing.Size(122, 89);
-            this.btnDivide.TabIndex = 8;
-            this.btnDivide.Text = "/";
-            this.btnDivide.UseVisualStyleBackColor = false;
-            // 
-            // btnShowInfo
-            // 
-            this.btnShowInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShowInfo.Location = new System.Drawing.Point(42, 69);
-            this.btnShowInfo.Name = "btnShowInfo";
-            this.btnShowInfo.Size = new System.Drawing.Size(236, 119);
-            this.btnShowInfo.TabIndex = 0;
-            this.btnShowInfo.Text = "Show Info";
-            this.btnShowInfo.UseVisualStyleBackColor = true;
+            this.tbInfo.Location = new System.Drawing.Point(42, 225);
+            this.tbInfo.Multiline = true;
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.Size = new System.Drawing.Size(527, 454);
+            this.tbInfo.TabIndex = 2;
+            this.tbInfo.Text = "This application is a simple calculator that has the features of adding, subtract" +
+    "ing, multiplying and dividing two numbers.\r\n\r\nMade by Meyra!";
+            this.tbInfo.Visible = false;
             // 
             // btnHideInfo
             // 
@@ -186,14 +191,19 @@
             this.btnHideInfo.TabIndex = 1;
             this.btnHideInfo.Text = "Hide Info";
             this.btnHideInfo.UseVisualStyleBackColor = true;
+            this.btnHideInfo.Visible = false;
+            this.btnHideInfo.Click += new System.EventHandler(this.btnHideInfo_Click);
             // 
-            // textBox1
+            // btnShowInfo
             // 
-            this.textBox1.Location = new System.Drawing.Point(42, 225);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(527, 454);
-            this.textBox1.TabIndex = 2;
+            this.btnShowInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowInfo.Location = new System.Drawing.Point(42, 69);
+            this.btnShowInfo.Name = "btnShowInfo";
+            this.btnShowInfo.Size = new System.Drawing.Size(236, 119);
+            this.btnShowInfo.TabIndex = 0;
+            this.btnShowInfo.Text = "Show Info";
+            this.btnShowInfo.UseVisualStyleBackColor = true;
+            this.btnShowInfo.Click += new System.EventHandler(this.btnShowInfo_Click);
             // 
             // Form1
             // 
@@ -226,7 +236,7 @@
         private System.Windows.Forms.Button btnDivide;
         private System.Windows.Forms.Button btnMultiply;
         private System.Windows.Forms.Button btnSubtract;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbInfo;
         private System.Windows.Forms.Button btnHideInfo;
         private System.Windows.Forms.Button btnShowInfo;
     }
--- PCS1/Week1/SimpleCalculator/SimpleCalculator/Form1.cs
+++ PCS1/Week1/SimpleCalculator/SimpleCalculator/Form1.cs
@@ -16,5 +16,50 @@ namespace SimpleCalculator
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int answer = Convert.ToInt32(this.tbFirstNumber.Text) + Convert.ToInt32(this.tbSecondNumber.Text);
+            this.lblAnswer.Text = "Answer: " + answer;
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            int answer = Convert.ToInt32(this.tbFirstNumber.Text) - Convert.ToInt32(this.tbSecondNumber.Text);
+            this.lblAnswer.Text = "Answer: " + answer;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            int answer = Convert.ToInt32(this.tbFirstNumber.Text) * Convert.ToInt32(this.tbSecondNumber.Text);
+            this.lblAnswer.Text = "Answer: " + answer;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(this.tbSecondNumber.Text);
+            int num2 = Convert.ToInt32(this.tbSecondNumber.Text);
+            if (num2 != 0)
+            {
+                int answer = num1 / num2;
+                this.lblAnswer.Text = "Answer: " + answer;
+            }
+            else
+            {
+                this.lblAnswer.Text = "Division by 0 is not allowed!";
+            }
+        }
+
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            this.btnHideInfo.Visible = true;
+            this.tbInfo.Visible = true;
+        }
+
+        private void btnHideInfo_Click(object sender, EventArgs e)
+        {
+            this.btnHideInfo.Visible = false;
+            this.tbInfo.Visible = false;
+        }
     }
 }

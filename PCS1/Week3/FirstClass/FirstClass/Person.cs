8b4a2c80a2963a69dff4ef968fed3198aa51a8c7
Meyra Al Saleh <meyrasaleh@gmail.com>
2025-06-17 15:47:04 +0300

    PCS1-week3: Completed assignment 1

---
+++ PCS1/Week3/FirstClass/FirstClass/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    class Person
+    {
+        private String name;
+        private int age;
+
+        public void SetName(String name)
+        {
+            this.name = name;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+    }
+}

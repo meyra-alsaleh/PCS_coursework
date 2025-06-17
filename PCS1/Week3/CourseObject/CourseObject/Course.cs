6573f5376628e45a53dcb79d454900deb9bd4ed2
Meyra Al Saleh <meyrasaleh@gmail.com>
2025-06-17 15:54:33 +0300

    PCS1-week3: Start assignment 2 set-up

---
+++ PCS1/Week3/CourseObject/CourseObject/Course.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseObject
+{
+    public class Course
+    {
+
+        // Instance variables 
+        private String name;
+        private int ec;
+        // Methods 
+        public void SetName(String name)
+        { this.name = name; }
+        public String GetName()
+        { return this.name; }
+
+        public void SetEc(int ec)
+        { this.ec = ec; }
+        public int GetEc()
+        { return this.ec; }
+
+    }
+}

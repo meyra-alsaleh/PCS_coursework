233849ad364b7cb9d296b16188d51a939d2eeb6c
Meyra Al Saleh <meyrasaleh@gmail.com>
2025-06-21 15:17:50 +0300

    PCS1-week3: Assignment 3 set-up form design and private class car fields and methods

---
+++ PCS1/Week3/CarSimulation/CarSimulation/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSimulation
+{
+    internal class Car
+    {
+        // Instance variables
+        private string brand;
+        private int currentSpeed;
+        private int maxSpeed;
+
+
+
+        // Mehods
+        public void SetBrand(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public void SetMaxSpeed(int maxSpeed)
+        {
+            this.maxSpeed = maxSpeed;
+        }
+
+        public string AsAString()
+        {
+            return "Speed of my " + this.brand + " is " + this.currentSpeed + 
+                " km/h and its maximum speed is " + this.maxSpeed + " km/h";
+        }
+
+    }
+}
---
+++ PCS1/Week3/CarSimulation/CarSimulation/Form1.cs
@@ -0,0 +1,15 @@
+namespace CarSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccelerate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
